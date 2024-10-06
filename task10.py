1f094f4167d2174ddd8f4600113c74ac646b1732
Максим Постников <maksimpostnikov78@gmail.com>
2024-10-05 23:56:35 -0700

    Сделал 5,8,9,10,11

--- task10.py
+++ task10.py
@@ -1,5 +1,5 @@
 def a(x1, y1, x2, y2):
-    return abs(x2 - x1) == abs(y2 - y1)
+    return (x1 == x2) or (y1 == y2) or (abs(x2 - x1) == abs(y2 - y1))
 x1, y1, x2, y2 = map(int, input("Введите координаты клеток (x1 y1 x2 y2): ").split())
 if a(x1, y1, x2, y2):
     print("YES")
--- task5.py
+++ task5.py
@@ -1,1 +1,9 @@
-print(*filter(lambda a: a % 133 == 125 and a % 134 == 111, range(10000, 100000)))
+def find_numbers():
+    results = []
+    for n in range(10000, 100000):
+        if n % 133 == 125 and n % 134 == 111:
+            results.append(n)
+    return results
+numbers = find_numbers()
+print(numbers)
+
